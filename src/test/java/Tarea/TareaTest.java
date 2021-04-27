75f311ae2076e3a1395c94d44a600e724129be8d
Xavi <al386135@uji.es>
2021-04-27 19:17:35 +0200

    Merge branch 'master' of D:\Descargas Internet\Asignaturas 2º\Programación avanzada\ProyectoContinuo with conflicts.

---
+++ src/test/java/Tarea/TareaTest.java
@@ -0,0 +1,64 @@
+package Tarea;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TareaTest {
+
+    @Test
+    void finalizarTarea() {
+    }
+
+    @Test
+    void introducirPersonal() {
+    }
+
+    @Test
+    void eliminarPersonal() {
+    }
+
+    @Test
+    void getTitulo() {
+    }
+
+    @Test
+    void getListPersona() {
+    }
+
+    @Test
+    void isFinalizado() {
+    }
+
+    @Test
+    void getResEsperado() {
+    }
+
+    @Test
+    void getNombrePersonal() {
+    }
+
+    @Test
+    void asignarResponsable() {
+    }
+
+    @Test
+    void getResponsable() {
+    }
+
+    @Test
+    void getfCreacion() {
+    }
+
+    @Test
+    void getfFinal() {
+    }
+
+    @Test
+    void getClave() {
+    }
+
+    @Test
+    void getLista() {
+    }
+}
