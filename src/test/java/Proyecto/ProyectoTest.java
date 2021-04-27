f6b34507fa39b6b35483eecb060a974e6d125837
Xavi <al386135@uji.es>
2021-04-27 19:19:26 +0200

    ProyectoTest

---
+++ src/test/java/Proyecto/ProyectoTest.java
@@ -0,0 +1,28 @@
+package Proyecto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProyectoTest {
+
+    @Test
+    void añadirPersonal() {
+    }
+
+    @Test
+    void añadirTarea() {
+    }
+
+    @Test
+    void getTareas() {
+    }
+
+    @Test
+    void getListPersonas() {
+    }
+
+    @Test
+    void getListPersonasNombre() {
+    }
+}
